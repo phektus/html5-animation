abae5fbdbfd99c30fdeea2f78c36dfb24c7e0376
Arbie Samong <phektus@gmail.com>
2014-04-05 10:37:27 +0800

    initial commit

---
+++ utils.js
@@ -0,0 +1,14 @@
+var utils = {};
+
+// ensure we have requestAnimationFrame
+if(!window.requestAnimationFrame) {
+    window.requestAnimationFrame = (
+        window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function(callback) {
+                return window.setTimeout(callback, 1000/60);
+            }
+    );
+}
